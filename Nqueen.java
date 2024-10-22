e324b0a8ccfce7a3e2bf13f76128372f8c63f8d8
raaviabhiram <97377346+raaviabhiram@users.noreply.github.com>
2024-10-22 08:03:39 +0530

    Add files via upload

---
+++ Nqueen.java
@@ -0,0 +1,66 @@
+public class Nqueen {
+    public static void main(String[] args) {
+        int n = 4 ;
+        boolean[][] board = new boolean[n][n];
+        //probQueen(board , 0);
+        System.out.println(probQueen(board , 0));
+    }
+
+    public static int probQueen(boolean[][] board , int row){
+        if(row == board.length){
+            display(board);
+            System.out.println(" ");
+            return 1 ;
+        }
+        int count = 0;
+        for(int col = 0;col<board.length;col++){
+            if(isSafe(board , row , col)){
+                board[row][col] = true ;
+                count = count + probQueen(board , row+1);
+                board[row][col] = false ;
+            }
+
+        }
+
+        return count ;
+    }
+
+
+    private static boolean isSafe(boolean[][] board , int row , int col){
+        for(int i =0;i<row;i++){
+            if(board[i][col]){
+                return false ;
+            }
+        }
+
+        int maxLeft = Math.min(row , col);
+        for(int i =1;i<=maxLeft;i++){
+            if(board[row-1][col-1]){
+                return false ;
+            }
+        }
+
+        int maxRight = Math.min(row , board.length-col-1);
+        for(int i =1 ; i<=maxRight;i++){
+            if(board[row-1][col+1]){
+                return false ;
+            }
+        }
+
+        return true ;
+    }
+
+    private static void display(boolean[][] board){
+        for(boolean[] row : board){
+            for(boolean element : row){
+                if(element){
+                    System.out.print("Q ");
+                }
+                else{
+                    System.out.print("X ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
